3af2d3fd976fd06f57f349dac47959f3a2712e51
Immanuel Salazar Diaz <immanuel.salazar@wizeline.com>
2022-03-29 13:44:54 -0600

    Setup theme

--- app/src/main/java/com/wizeline/compose/academy/ui/theme/Color.kt
+++ app/src/main/java/com/wizeline/compose/academy/ui/theme/Color.kt
@@ -5,4 +5,7 @@ import androidx.compose.ui.graphics.Color
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+
+val PrimaryBlue = Color(0XFF31bdab)
+val PrimaryBlueVariant = Color(0XFFbedede)
+val secondaryBlue = Color(0xFF03DAC5)
