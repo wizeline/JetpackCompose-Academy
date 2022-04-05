7ff46bf1b30d84a0ecb19939b5891f7646ecbbf9
Immanuel Salazar Diaz <immanuel.salazar@wizeline.com>
2022-04-05 16:32:02 -0500

    update colors and shapes

--- app/src/main/java/com/wizeline/compose/academy/ui/theme/Color.kt
+++ app/src/main/java/com/wizeline/compose/academy/ui/theme/Color.kt
@@ -5,3 +5,6 @@ import androidx.compose.ui.graphics.Color
 val PrimaryBlue = Color(0XFF31bdab)
 val PrimaryBlueVariant = Color(0XFFbedede)
 val secondaryBlue = Color(0xFF03DAC5)
+val PrimaryGray =  Color(0XFF444544)
+val PrimaryGravyVariant =  Color(0XFF444544).copy(alpha = 0.5f)
+val PrimaryOrange = Color(0XFFD97119)
