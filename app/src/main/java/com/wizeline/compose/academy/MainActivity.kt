a7ba04d49865c5f8ce5db657d64866b0313f9855
Immanuel Salazar Diaz <immanuel.salazar@wizeline.com>
2022-04-05 16:23:55 -0500

    update files

--- app/src/main/java/com/wizeline/compose/academy/MainActivity.kt
+++ app/src/main/java/com/wizeline/compose/academy/MainActivity.kt
@@ -3,12 +3,14 @@ package com.wizeline.compose.academy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.wizeline.compose.academy.ui.theme.ComposeAcademyTheme
 
@@ -31,7 +33,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Image(
+        painter = painterResource(id = R.drawable.landing_2),
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.fillMaxSize()
+    )
 }
 
 @Preview(showBackground = true)
