7dbaff6bada9e0d4c406bc6ac309145042c00368
Immanuel Salazar Diaz <immanuel.salazar@wizeline.com>
2022-04-05 10:33:10 -0500

    add resources

--- app/src/main/java/com/wizeline/compose/academy/ui/theme/Theme.kt
+++ app/src/main/java/com/wizeline/compose/academy/ui/theme/Theme.kt
@@ -17,15 +17,6 @@ private val LightColorPalette = lightColors(
     primary = PrimaryBlue,
     primaryVariant = PrimaryBlueVariant,
     secondary = secondaryBlue
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable
